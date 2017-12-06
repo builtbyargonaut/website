fa6259fefb16489327585812b9d5b992b26a1441
Thomas Chen <chen.thomas.c@gmail.com>
2017-12-06 13:10:27 -0500

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Link from "gatsby-link";
+
+const IndexPage = () => (
+  <div className="index">
+    <h1>Argonaut is open for business. 🚀</h1>
+    <p>
+      <strong>Argonaut</strong> is a software development consultancy run by me,{" "}
+      <strong>Tom Chen</strong>.
+    </p>
+    <p>
+      I'm passionate about building fast, robust, and secure web applications
+      that deliver extraordinary user experiences. I love working on interesting
+      projects with good people.
+    </p>
+    <p>
+      A new website is in the works. In the meantime, you can find me on{" "}
+      <a href="https://github.com/thomascchen">GitHub</a>,{" "}
+      <a href="https://www.twitter.com/tchen">Twitter</a>, and{" "}
+      <a href="https://www.linkedin.com/in/chenthomasc/">LinkedIn</a>.
+    </p>
+  </div>
+);
+
+export default IndexPage;
