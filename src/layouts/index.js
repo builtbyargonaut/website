9cdd4acc2ddb3133f3bcabf234993b18e1ced386
Thomas Chen <chen.thomas.c@gmail.com>
2018-01-12 11:24:42 -0500

    Add email and clean up

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import React from "react";
+import PropTypes from "prop-types";
+import Link from "gatsby-link";
+import Helmet from "react-helmet";
 
-import './index.css'
+import "./index.css";
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Argonaut" />
+    <Helmet title="Argonaut — Web Development Consulting" />
     {children()}
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
+  children: PropTypes.func
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const IndexPage = () => (
   <div className="index">
-    <h1>Argonaut is open for business. 🚀</h1>
+    <h1>Argonaut is open for business 🚀</h1>
     <p>
       <strong>Argonaut</strong> is a software development consultancy run by me,{" "}
       <strong>Tom Chen</strong>.
@@ -10,10 +10,13 @@ const IndexPage = () => (
     <p>
       I'm passionate about building fast, robust, and secure web applications
       that deliver extraordinary user experiences. I love working on interesting
-      projects with good people—get in touch.
+      projects with good people—<a href="mailto:tom@builtbyargonaut.com">
+        get in touch
+      </a>.
     </p>
     <p>
-      You can find me on <a href="https://github.com/thomascchen">GitHub</a>,{" "}
+      You can also find me on{" "}
+      <a href="https://github.com/thomascchen">GitHub</a>,{" "}
       <a href="https://www.twitter.com/tchen">Twitter</a>, and{" "}
       <a href="https://www.linkedin.com/in/chenthomasc/">LinkedIn</a>.
     </p>
