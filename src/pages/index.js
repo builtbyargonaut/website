68c7c624350b209183dfda56b7ea07b588612bf3
Thomas Chen <chen.thomas.c@gmail.com>
2018-03-08 20:06:21 -0500

    Use the right single quotation mark
    See https://en.wikipedia.org/wiki/Modifier_letter_apostrophe

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
 const IndexPage = () => (
   <div className="index">
-    <h1>Let's Build Together 🏗</h1>
+    <h1>Let’s Build Together 🏗</h1>
     <p>
-      <strong>Argonaut</strong> is a software development consultancy run by me,{" "}
+      <strong>Argonaut</strong> is a software development consultancy run by me,{' '}
       <strong>Tom Chen</strong>.
     </p>
     <p>
@@ -13,9 +13,9 @@ const IndexPage = () => (
       people—<a href="mailto:tom@builtbyargonaut.com">get in touch</a>.
     </p>
     <p>
-      You can also find me on{" "}
-      <a href="https://github.com/thomascchen">GitHub</a>,{" "}
-      <a href="https://www.twitter.com/tchen">Twitter</a>, and{" "}
+      You can also find me on{' '}
+      <a href="https://github.com/thomascchen">GitHub</a>,{' '}
+      <a href="https://www.twitter.com/tchen">Twitter</a>, and{' '}
       <a href="https://www.linkedin.com/in/chenthomasc/">LinkedIn</a>.
     </p>
   </div>
